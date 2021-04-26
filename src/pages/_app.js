4c04f3f162a08c8ef54db73a1b173165ceb00074
pmenta <jvgamerofficial@gmail.com>
2021-04-26 06:34:01 -0300

    dark mode update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,14 +2,14 @@ import '../styles/global.scss'
 
 import { Header } from '../components/Header'
 import { Player } from '../components/Player'
-import {PlayerContext} from '../contexts/PlayerContext'
+import { PlayerContext } from '../contexts/PlayerContext'
 
 import styles from '../styles/app.module.scss'
 import { useState } from 'react'
 
 
 function MyApp({ Component, pageProps }) {
-
+  
   const [episodeList, setEpisodeList] = useState([])
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
   const [isPlaying, setIsPlaying] = useState(false)
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,6 +9,13 @@ export default class MyDocument extends Document {
                     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet"/>
                     
                     <link rel="icon" type="image/png" href="/favicon.png" />
+
+                    <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
+
+                    <link rel='manifest' href='/manifest.json'></link>
+                    <link rel='shortcut icon' href='/apple-icon.png'></link>
+                    <link rel='icon' href='/favicon.png'></link>
+
                 </Head>
                 <body>
                     <Main />
